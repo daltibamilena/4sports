2b92a0985424c00dab702371f71b75350f72bc2b
Milena Daltiba <daltibamilena@gmail.com>
2022-03-09 18:43:25 -0400

    feat: add user admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@
     price: Faker::Commerce.price,
     image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ["sports"]))
 end
+
+User.create(email: "admin@example.com", password: "123456", password_confirmation: "123456", admin: true)
